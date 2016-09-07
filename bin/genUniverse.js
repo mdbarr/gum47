88078224329c9bba94b4821d01033c615079f9c6
Mark Barr <mark@hyperingenuity.com>
2016-09-07 00:10:20 -0400

    Syllabic planetary name generation added.

--- bin/genUniverse.js
+++ bin/genUniverse.js
@@ -15,7 +15,7 @@ var SYSTEM_COUNT = 1000000;
 var BOUNDARY = 10000;
 var BOUNDARY_ = -1 * BOUNDARY;
 
-var BLACKHOLE_RADIUS = 35;
+var BLACKHOLE_RADIUS = 30;
 
 var INITIAL_POS = 800; //Math.max(SYSTEM_COUNT * 0.001, 40);
 
@@ -40,26 +40,57 @@ function inCircle(center_x, center_y, radius, x, y) {
   return dist <= Math.pow(radius, 2);
 }
 
-function generateName(length) {
-  length = length || 8;
-  var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-  var name = '';
-  for (var i = 0; i < length; i++) {
-    name += lowercase.charAt(random(0, lowercase.length));
-    if (i === 0) {
-      name = name.toUpperCase();
+var SYLLABLES = ["a", "ab", "ad", "af", "ag", "ah", "ak", "al", "am", "an", "ang", "ap", "ar", "as", "ash", "at", "ath", "av", "aw", "ay", "az", "ba", "be", "bi", "bla", "ble", "bli", "blo", "blu", "bo", "bra", "bre", "bri", "bro", "bru", "bu", "ce", "cla", "cle", "cli", "clo", "clu", "cra", "cro", "cru", "da", "de", "di", "do", "dra", "dre", "dri", "dro", "dru", "du", "e", "eb", "ed", "ef", "eg", "eh", "ek", "el", "em", "en", "eng", "ep", "er", "es", "esh", "et", "eth", "ev", "ew", "ey", "ez", "fa", "fe", "fi", "fla", "fle", "fli", "flo", "flu", "fo", "fra", "fre", "fri", "fro", "fru", "fu", "ga", "ge", "gi", "gla", "gle", "gli", "glo", "glu", "go", "gra", "gre", "gri", "gro", "gru", "gu", "ha", "he", "hi", "ho", "hu", "i", "ib", "id", "if", "ig", "ih", "ik", "il", "im", "in", "ing", "ip", "ir", "is", "ish", "it", "ith", "iv", "iw", "iy", "iz", "ka", "ke", "ki", "kla", "kle", "kli", "klo", "klu", "ko", "kra", "kre", "kri", "kro", "kru", "ku", "la", "le", "li", "lo", "lu", "ly", "ma", "me", "mi", "mo", "mu", "na", "ne", "ni", "no", "nu", "o", "ob", "od", "of", "og", "oh", "ok", "ol", "om", "on", "ong", "op", "or", "os", "osh", "ot", "oth", "ov", "ow", "oy", "oz", "pa", "pe", "pi", "pla", "ple", "pli", "plo", "plu", "po", "pra", "pre", "pri", "pro", "pru", "pu", "ra", "re", "ri", "ro", "ru", "sa", "se", "sha", "she", "shi", "sho", "shu", "si", "sla", "sle", "sli", "slo", "slu", "so", "su", "ta", "te", "tha", "the", "thi", "tho", "thra", "thre", "thri", "thro", "thru", "thu", "thy", "ti", "to", "tra", "tre", "tri", "tro", "tru", "tu", "u", "ub", "ud", "uf", "ug", "uh", "uk", "ul", "um", "un", "ung", "up", "ur", "us", "ush", "ut", "uth", "uv", "uw", "uy", "uz", "va", "ve", "vi", "vla", "vle", "vli", "vlo", "vlu", "vo", "vra", "vre", "vri", "vro", "vru", "vu", "wa", "we", "wi", "wo", "wra", "wre", "wri", "wro", "wru", "wu", "ya", "ye", "yi", "yl", "yo", "yth", "yu", "za", "ze", "zi", "zla", "zle", "zli", "zlo", "zlu", "zo", "zra", "zre", "zri", "zro", "zru", "zu"];
+
+var NAME_HASH = { };
+
+function generateWord(length) {
+  var syllableCount = length || random(2, 7);
+
+  var word = '';
+  for (var i = 0; i < syllableCount; i++) {
+    word += SYLLABLES[random(0, SYLLABLES.length - 1)];
+  }
+  word = word.charAt(0).toUpperCase() + word.slice(1);
+
+  return word;
+}
+
+function generateName() {
+  var clash = true;
+  var name;
+
+
+  while (clash)  {
+
+    name = generateWord(); //+ ' ' + generateWord();
+
+    clash = (NAME_HASH[name]) ? true : false;
+
+    if(clash) {
+      //console.log('CLASH', name);
     }
   }
+  NAME_HASH[name] = true;
+  //console.log(name);
   return name;
 }
 
-var STELLAR_TYPES = [ { class: 'O', color: [ 50, 50, 255 ], size: 'giant', occurrence: 0.0000003 },
-                      { class: 'B', color: [ 100, 100, 255 ], size: 'giant', occurrence: 0.00125 },
-                      { class: 'A', color: [ 151, 200, 232 ], size: 'normal', occurrence: 0.00625},
-                      { class: 'F', color: [ 255, 255, 255 ], size: 'normal', occurrence: 0.0303 },
-                      { class: 'G', color: [ 200, 200, 0 ], size: 'normal', occurrence: 0.075 },
-                      { class: 'K', color: [ 255, 127, 0 ], size: 'normal', occurrence: 0.12, },
-                      { class: 'M', color: [ 200, 0, 0 ], size: 'giant', occurrence: 0.76 } ];
+var STELLAR_TYPES = [ { class: 'O', size: 'giant', occurrence: 0.0000003 },
+                      { class: 'B', size: 'giant', occurrence: 0.00125 },
+                      { class: 'A', size: 'normal', occurrence: 0.00625},
+                      { class: 'F', size: 'normal', occurrence: 0.0303 },
+                      { class: 'G', size: 'normal', occurrence: 0.075 },
+                      { class: 'K', size: 'normal', occurrence: 0.12, },
+                      { class: 'M', size: 'giant', occurrence: 0.76 } ];
+
+var STELLAR_COLORS = { 'O': [ 50, 50, 255 ],
+                       'B': [ 100, 100, 255 ],
+                       'A': [ 100, 100, 255 ],
+                       'F': [ 255, 255, 255 ],
+                       'G': [ 255, 255, 200 ],
+                       'K': [ 255, 187, 153 ],
+                       'M': [ 200, 0, 0 ] };
 
 function stellarGenerator() {
   var accuracy = 10000000;
@@ -74,14 +105,23 @@ function stellarGenerator() {
     }
   }
 
-  var sun = _.clone(STELLAR_TYPES[index]);
+  var stellarClass = STELLAR_TYPES[index].class;
+
+  var sun = {
+    class: stellarClass,
+    size: STELLAR_TYPES[index].size,
+    color: STELLAR_COLORS[stellarClass]
+  };
   return sun;
 }
 
+var PLANET_NUMERALS = [ 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X' ];
+
 function planetGenerator(system, index) {
   var planet = {
-    name: system.name + ' ' + (index + 1)
+    name: system.name + ' ' + PLANET_NUMERALS[index]
   };
+
   return planet;
 }
 
@@ -144,7 +184,7 @@ function Universe() {
   var initialY = INITIAL_POS;//random(BOUNDARY_, BOUNDARY);
   var initial = systemGenerator(initialX, initialY);
 
-  var printPercentages = [ 0.25, 0.5, 0.75, 1 ];
+  var printPercentages = [ 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1 ];
 
   console.log(initialX, initialY);
 
